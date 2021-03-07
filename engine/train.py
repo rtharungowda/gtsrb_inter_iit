680c3b54a3cb90624d99829a440687d4e095a88c
rtharungowda <rtharun.gowda.cd.ece19@itbhu.ac.in>
2021-03-07 18:19:42 +0000

    Move: calc_mean_std to tools

--- engine/train.py
+++ engine/train.py
@@ -1,28 +1,15 @@
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
-# import albumentations as A
-# from albumentations.pytorch.transforms import ToTensorV2
-
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from PIL import Image
 
 import numpy as np
+import sys
+sys.path.insert(1,'/content/gtsrb_inter_iit/utils')
 
-def calc_mean_std(df):
-    imgs = []
-    for ind,row in df.iterrows():
-        print(row['path'])
-        img = Image.open(row['path'])
-        img = img.resize((30,40))
-        img_ar = np.array(img)
-        imgs.append(img_ar)
-        
-    ar = np.array(imgs)
-    mean, std = ar.mean(axis=0), ar.std(axis=0)
-    print(mean,std)
-    return mean ,std
+from tools import calc_mean_std
 
 class GTSRB(Dataset):
     def __init__(self,
